d08cb7e60cd9b46dfae8c2559ef1f32ad72d4f10
Hauptling12 <rdserver1@outlook.com>
2022-06-26 13:36:37 +1000

    move software to beta

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ALPHA SOFTWARE
 ## alarm clock program
+### man page
+-v shows version
+-h shows help page
+-l lists alarms
+#### to add alarms
+open ~/.local/share/alarm/alarm.json with your text editor
+and add time json ex "time":"2022/06/26:13:18:20"
--- main.js
+++ main.js
@@ -1,44 +1,18 @@
 var spawn = require("child_process").spawn;
-const readline = require('readline');
-// var array = ["2021-12-1:10:20:00","2221-10-1:10:20:00"]
-// var inc = array.includes('2021-12-1:10:20:00')
-// console.log(inc);
-var test = require('./array.json');
+var test = require(db_file);
+var music = "/home/" + process.env.USER + "/.local/share/alarm/alarm.wav"
 var db_file = "/home/" + process.env.USER + "/.local/share/alarm/alarms.json"
-var out = test.some(item => item.time === '2021-12-1:10:20:00');
-// var result = test.filter(x => x.time === "2021-12-1:10:20:00");
-// console.log(result);
-const date = new Date();
-// Function to convert
-// single digit input
-// to two digits
-const formatData = (input) => {
-  if (input > 9) {
-    return input;
-  } else return `0${input}`;
-};
+function addZero(i) {
+    if (i < 10) {i = "0" + i}
+    return i;
+}
 
-// Function to convert
-// 24 Hour to 12 Hour clock
-const formatHour = (input) => {
-  if (input > 12) {
-    return input - 12;
-  }
-  return input;
-};
-
-const format = {
-  dd: formatData(date.getDate()),
-  mm: formatData(date.getMonth() + 1),
-  yyyy: date.getFullYear(),
-  HH: formatData(date.getHours()),
-  hh: formatData(formatHour(date.getHours())),
-  MM: formatData(date.getMinutes()),
-  SS: formatData(date.getSeconds()),
-};
 switch (process.argv[2]) {
     case '-h':
         console.log("-h help");
+        break;
+    case '-v':
+        console.log('0.0.1');
         break;
     case '-a':
         spawn(process.env.EDITOR, [db_file]);
@@ -47,32 +21,16 @@ switch (process.argv[2]) {
         console.table(test);
         break;
     default:
-        const format24Hour = ({ dd, mm, yyyy, HH, MM, SS }) => {
-            console.log(`${mm}/${dd}/${yyyy}-${HH}:${MM}:${SS}`);
-        };
-        format24Hour(format);
-        console.log(out);
-        var ou = console.log(out === "true");
-        console.log(ou)
-        // readline.emitKeypressEvents(process.stdin);
-        // process.stdin.setRawMode(true);
-        // process.stdin.on('keypress', (str, key) => {
-
-    // // "Raw" mode so we must do our own kill switch
-        //     if(key.sequence === '\u0003') {
-        //         process.exit();
-        //     }
-        //     if(key.sequence === 'q') {
-        //         process.exit();
-        //     }
-
-    // // User has triggered a keypress, now do whatever we want!
-    // // ...
-        // });
-        // while (true) {
-        //     if (out === true) {
-        //         console.log("dsdsd");
-        //         break;
-        //     }
-        // }
+        while (true) {
+    const d = new Date();
+    let ho = addZero(d.getHours());
+    let mi = addZero(d.getMinutes());
+    let se = addZero(d.getSeconds());
+    let ye = addZero(d.getFullYear());
+    let da = addZero(d.getDate());
+    let mo = addZero(d.getMonth() +1 );
+        let timewq = ye + "/" + mo + "/" + da + ":" + ho + ":" + mi + ":" + se;
+        if (test.some(item => item.time === timewq)===true) break;
+        }
+        spawn('mpv', [music]);
 }
